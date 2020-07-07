15c7e9b12697e9fea3021ea5dacfa5309e8294ae
bbz94 <kristians.voronics@gmail.com>
2020-07-07 14:23:23 +0300

    sync

---
+++ run.sh
@@ -0,0 +1,6 @@
+cd $HOME
+rm -rf Ansible-Vpc
+git clone https://github.com/bbz94/Ansible-Vpc
+cd Ansible-Vpc
+#ansible-playbook -i ansible_hosts gce-instances.yml
+ansible-playbook -e "subnetname=testvpc2 subnetaddress=10.164.4.48/28 sharedvpcname=aisarch-common-host sharedvpcprojectid=aisarch-common-host networkuser=kristians.voronics@if.lv serviceprojectid=aisarch-test-project" -i ansible_hosts gce-instances.yml
