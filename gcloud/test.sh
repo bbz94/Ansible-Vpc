ff75a69add10114eef9aba1d2fcb7176e93ba2cc
bbz94 <kristians.voronics@gmail.com>
2020-07-08 17:19:14 +0300

    s

--- gcloud/test.sh
+++ gcloud/test.sh
@@ -6,7 +6,6 @@ networkuser=$4
 subnetname=$5
 region=$6
 replace_user=kristians.voronics@if.lv
-replace_etag=BwWp6v8fxEo=
 subnet_policy_path=/root/Ansible-Vpc/subnet-policy.json 
 
 
@@ -15,11 +14,11 @@ cd "/mnt/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin"
 bash gcloud auth activate-service-account id-vpc-network-peering@aisarch-security-hub-prod.iam.gserviceaccount.com --key-file=$credentials_file
 
 # Attach project to shared VPC
-bash gcloud compute shared-vpc associated-projects add $serviceprojectid --host-project=$sharedvpcprojectid
+etag=$(bash gcloud compute networks subnets get-iam-policy $subnetname --region $region --project $sharedvpcprojectid --format json | jq .etag)
 
 # Service Project Admins for all subnets
 sed -i "s/replace_user/$replace_user/g" $subnet_policy_path
-sed -i "s/replace_etag/$replace_etag/g" $subnet_policy_path
+sed -i "s/replace_etag/$etag/g" $subnet_policy_path
 bash gcloud compute networks subnets set-iam-policy $subnetname $subnet_policy_path --region $region --project $sharedvpcprojectid
 
 echo $credentials_file
@@ -30,3 +29,5 @@ echo $subnetname
 echo $region
 echo $replace_user
 echo $subnet_policy_path
+
+
