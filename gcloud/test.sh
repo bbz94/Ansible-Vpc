7b0326db601151491ee28096f207fdae578fc765
bbz94 <kristians.voronics@gmail.com>
2020-07-08 09:24:53 +0300

    s

--- gcloud/test.sh
+++ gcloud/test.sh
@@ -3,6 +3,7 @@ credentials_file=$1
 project=$2
 subnetname=$3
 
+cd "/mnt/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin"
 gcloud auth activate-service-account id-vpc-network-peering@aisarch-security-hub-prod.iam.gserviceaccount.com --key-file=$credentials_file
 
 echo $credentials_file
