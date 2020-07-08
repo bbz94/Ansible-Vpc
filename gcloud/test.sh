0f8c488b8ba16ac40d76af0301e7d8b431df9b32
bbz94 <kristians.voronics@gmail.com>
2020-07-08 09:42:18 +0300

    s

--- gcloud/test.sh
+++ gcloud/test.sh
@@ -1,10 +1,17 @@
 # Variables
 credentials_file=$1
-project=$2
-subnetname=$3
+sharedvpcprojectid=$2
+serviceprojectid=$3
+subnetname=$4
 
+# Auth
 cd "/mnt/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin"
 bash gcloud auth activate-service-account id-vpc-network-peering@aisarch-security-hub-prod.iam.gserviceaccount.com --key-file=$credentials_file
 
+# Attach project to shared VPC
+gcloud compute shared-vpc associated-projects add $serviceprojectid --host-project=$sharedvpcprojectid
+
 echo $credentials_file
-echo $project
+echo $sharedvpcprojectid
+echo $serviceprojectid
+echo $subnetname
