2af794b49594e890e9c543a91498fff9ebd74f26
bbz94 <kristians.voronics@gmail.com>
2020-07-09 09:08:31 +0300

    s

--- gcloud/shared_vpc_automaiton.sh
+++ gcloud/shared_vpc_automaiton.sh
@@ -7,12 +7,14 @@ subnetname=$5
 region=$6
 subnet_policy_path=/root/Ansible-Vpc/subnet-policy.json 
 
-
 # Auth
 cd "/mnt/c/Program Files (x86)/Google/Cloud SDK/google-cloud-sdk/bin"
 bash gcloud auth activate-service-account id-vpc-network-peering@aisarch-security-hub-prod.iam.gserviceaccount.com --key-file=$credentials_file
 
 # Attach project to shared VPC
+gcloud compute shared-vpc associated-projects add $serviceprojectid --host-project=$sharedvpcprojectid
+
+# Get e tag for subnet
 etag=$(bash gcloud compute networks subnets get-iam-policy $subnetname --region $region --project $sharedvpcprojectid --format json | jq .etag)
 
 # Service Project Admins for all subnets
