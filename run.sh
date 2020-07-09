8f8cd7f2b49db6a57ec8e9816d9b359a6c9e38d0
bbz94 <kristians.voronics@gmail.com>
2020-07-09 07:53:34 +0300

    s

--- run.sh
+++ run.sh
@@ -2,5 +2,4 @@ cd $HOME
 rm -rf Ansible-Vpc
 git clone https://github.com/bbz94/Ansible-Vpc
 cd Ansible-Vpc
-#ansible-playbook -i ansible_hosts gce-instances.yml
-ansible-playbook -e "subnetname=testvpc2 subnetaddress=10.164.4.48/28 sharedvpcname=aisarch-common-host sharedvpcprojectid=aisarch-common-host networkuser=user:kristians.voronics@if.lv serviceprojectid=aisarch-security-hub-prod region=europe-north1 zone=europe-north1-a" -i ansible_hosts gce-instances.yml
+ansible-playbook -e "subnetname=testvpc2 subnetaddress=10.164.4.48/28 sharedvpcname=aisarch-common-host sharedvpcprojectid=aisarch-common-host networkuser=user:kristians.voronics@if.lv serviceprojectid=aisarch-security-hub-prod region=europe-north1 zone=europe-north1-a auth_kind=serviceaccount credentials_file=/root/compute-video-demo-ansible/gce_vars/secret.json" -i ansible_hosts shared_vpc_automaiton.yml
